e068715e67206552f2e117e56776cca8e23662ea
kieuhuyyy <90268406+kieuhuyyy@users.noreply.github.com>
2021-09-07 22:34:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 const CONFIG = {
     titleWeb: "web này dành riêng cho LINH",
     introTitle: 'Nguyễn Ánh Linh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `thuc ra anh yeu em nhiều lắm luôn á `,
     btnIntro: '^^chào ny mình^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'yêu hoặc đốt phố xã đàn 🥰',
+    desc: 'Phải chăng a đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không thèm :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho kieuhuy <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Hết dịch, mình qua đón đi chơi nha.',
